ea00c73c25764a8b4513f97508902c97b4dc4339
Ayush Singh <sayhi.ayush@gmail.com>
2025-04-09 20:04:19 +0530

    User authentication added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,21 @@
 import express from 'express';
 import authRoutes from './routes/auth.route.js'
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser';
+
 import { connectDB } from './lib/db.js';
 
-dotenv.config()
+
+dotenv.config();
+
 const app = express();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
-app.use("/api/auth", authRoutes)
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, ()=>{
     console.log("Server running on Port: " + PORT);
