bf478ededf85d094945b1ee34ef593d6f25ed25b
Ayush Singh <sayhi.ayush@gmail.com>
2025-04-10 13:02:51 +0530

    message routes done

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import authRoutes from './routes/auth.route.js'
+import messageRoutes from './routes/message.route.js'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser';
 
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, ()=>{
     console.log("Server running on Port: " + PORT);
